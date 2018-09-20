43dcd4a6d55399a68329ab4e4fea2420480b3e93
Jason Heyer <heyerjt01@mounties.mansfield.edu>
2018-09-19 22:04:49 -0400

    Add initial LoginFrag.java in preparation for login flow.

--- app/src/main/java/com/mybarchips/barchips/MainActivity.java
+++ app/src/main/java/com/mybarchips/barchips/MainActivity.java
@@ -2,6 +2,7 @@ package com.mybarchips.barchips;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this, "Testing", Toast.LENGTH_LONG);
+        Toast.makeText(this, "Testing 1..", Toast.LENGTH_LONG);
+
     }
 }
